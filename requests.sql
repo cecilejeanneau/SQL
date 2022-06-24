70e605873c6826e0f14c408aea8d6ed16520ac6a
cecilejeanneau <cecile.n.jeanneau@gmail.com>
2022-06-24 12:59:13 +0000

    sql request+

--- requests.sql
+++ requests.sql
@@ -13,3 +13,8 @@ SELECT users.*,addresses.city FROM users JOIN addresses ON addresses.user_id = u
 -- selectionner all users and display on addresses table the city (JOIN)
 SELECT users.* FROM users JOIN addresses ON addresses.user_id = users.id WHERE addresses.number IS NULL
 -- La liste de tous les utilisateurs vivant à une adresse sans numéro ;
+SELECT * FROM products WHERE price > 1000
+-- liste de tous les products dont le prix est supérieur à 1000
+SELECT products.*,pictures.url FROM products JOIN pictures ON pictures.product_id = products.id
+-- selectionner all products and display on pictures table the url image (JOIN)
+SELECT products.* FROM products_categories JOIN products ON products_categories.product_id = products.id JOIN categories ON products_categories.product_id = categories.id WHERE categories.title = "Voyage"
