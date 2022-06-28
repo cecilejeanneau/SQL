05d2fa1722111f907bf401faf61e26abe5db314d
cecilejeanneau <cecile.n.jeanneau@gmail.com>
2022-06-28 12:01:19 +0000

    requests sql supp rapport mvc

--- requests.sql
+++ requests.sql
@@ -17,4 +17,15 @@ SELECT * FROM products WHERE price > 1000
 -- liste de tous les products dont le prix est supérieur à 1000
 SELECT products.*,pictures.url FROM products JOIN pictures ON pictures.product_id = products.id
 -- selectionner all products and display on pictures table the url image (JOIN)
-SELECT products.* FROM products_categories JOIN products ON products_categories.product_id = products.id JOIN categories ON products_categories.product_id = categories.id WHERE categories.title = "Voyage"
+SELECT products.* FROM products_categories JOIN products ON products_categories.product_id = products.id JOIN categories ON products_categories.category_id = categories.id WHERE categories.title = "Voyage"
+-- La liste de tous les produits appartenant à la catégorie « Voyage » ; produit id dans products_categories et category_id pour table categories where is Voyage!!!
+SELECT users.*, COUNT(*) FROM users JOIN orders ON users.id = orders.user_id GROUP BY user_id HAVING COUNT(*) > 10
+-- La liste de tous les utilisateurs ayant effectué plus de dix commandes dans orders compter le nombre de même id avec count dans tous les orders et having;
+SELECT products.* FROM products_orders JOIN products ON products_orders.product_id = products.id JOIN orders ON products_orders.order_id = orders.id JOIN users ON orders.user_id = users.id WHERE orders.user_id = (SELECT MIN(id) FROM users)
+-- La liste de tous les produits achetés par le premier utilisateur inscrit.
+
+
+
+
+php2_mvc :
+INSERT INTO `page` (`id`, `title`, `slug`, `content`) VALUES (NULL, 'Accueil', 'homepage', 'Bienvenue :)');
